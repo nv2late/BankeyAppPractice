b83771aa5e677efe2df7a25e17738ee6e67d76c8
reese <winterhazedev@gmail.com>
2022-08-04 01:48:03 +0900

    edit: add onboarding view controller

--- Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -5,4 +5,120 @@
 //  Created by Reese on 2022/08/04.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingContainerViewController: UIViewController {
+
+    let pageViewController: UIPageViewController
+    var pages = [UIViewController]()
+    // 현재 보여지는 VC가 어떤 것인지를 알려줌
+    var currentVC: UIViewController {
+        didSet {
+        }
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let page1 = ViewController1()
+        let page2 = ViewController2()
+        let page3 = ViewController3()
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        currentVC = pages.first!
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        
+        // 부모VC에 자식VC를 추가하는 방법(아래 세 줄)
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParent: self)
+        
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 부모VC의 가장자리에 맞춰서 위부터 페이지 레이아웃 고정
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ])
+        
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+        currentVC = pages.first!
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
+    // 이전 VC를 return
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+
+    // 다음 VC를 return
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+
+    /*
+     pages의 배열을 가져와서 인덱스 - 1이 0보다 같거나 크면 앞으로 이동하고 아니면 nil. 아래 함수도 다음일뿐 동일한 구조.
+     롤링할땐 guard문이, 현재 페이지에 고정됐을땐 currentVC에 할당된 값으로 이동한다.
+     */
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+}
+
+// MARK: - ViewControllers
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class ViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
