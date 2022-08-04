95ab370e387b1320a719c3e00fd17a76444f696b
reese <winterhazedev@gmail.com>
2022-08-04 22:04:43 +0900

    refactor: create generic onboarding view controller
    Generic Onboarding view controller can be reused across multiple onboarding screens.

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -17,9 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        //window?.rootViewController = LoginViewController()
-        //window?.rootViewController = OnboardingContainerViewController()
-        window?.rootViewController = OnboardingViewController()
+        window?.rootViewController = OnboardingContainerViewController()
         
         return true
     }
